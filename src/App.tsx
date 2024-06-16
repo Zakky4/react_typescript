45869aac25a814e713867def31663e89030ece2d
zakky <sadaoza@gmail.com>
2024-06-16 16:41:43 +0900

    Return Value type number

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Practice1 } from "./practices/Practice1";
+import { Practice2 } from "./practices/Practice2";
 
 function App() {
   return (
     <div className="App">
       <Practice1 />
+      <Practice2 />
     </div>
   );
 }
---
+++ src/practices/Practice2.tsx
@@ -0,0 +1,16 @@
+export const Practice2 = () => {
+  const getTotalFee = (num: number): number => {
+    const total = num * 1.1;
+    return total
+  };
+  const onClickPractice = () => { 
+    console.log(getTotalFee(1000));
+  };
+
+  return (
+    <div>
+      <p>練習問題：返却値の型指定</p>
+      <button onClick={onClickPractice}>練習問題２を実行</button>
+    </div>
+  );
+};
