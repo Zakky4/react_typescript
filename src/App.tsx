27c90af3273dd17428992bfbc29e577a5a683091
zakky <sadaoza@gmail.com>
2024-06-16 21:02:26 +0900

    Array Type

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import axios from "axios";
 import "./App.css";
+import { useState } from "react";
+import { Todo } from "./Todo";
+
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 function App() {
+const [todos, setTodos] = useState<Array<TodoType>>([]);
+
   const onClickFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      console.log(res);
+    axios.get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos").then((res) => {
+    setTodos(res.data);
     });
   };
 
   return (
     <div className="App">
-      <button onClick={onClickFetchData}>Fetch Data</button>
+      <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} userid={todo.userId} />
+      ))}
     </div>
   );
 }
---
+++ src/Todo.tsx
@@ -0,0 +1,4 @@
+export const Todo = (props) => {
+  const { title, userid } = props;
+  return <p>{`${title}(ユーザー:${userid})`}</p>;
+};
