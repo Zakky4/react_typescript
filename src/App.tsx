4f4a5a5e0c60856f83ceec4b84b2b35f181a9b8b
zakky <sadaoza@gmail.com>
2024-06-16 18:48:19 +0900

    Strict true

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Practice1 } from "./practices/Practice1";
 import { Practice2 } from "./practices/Practice2";
+import { Practice3 } from "./practices/Practice3";
 
 function App() {
   return (
     <div className="App">
       <Practice1 />
       <Practice2 />
+      <Practice3 />
     </div>
   );
 }
