4bc4eb1d9cd82d637842edefc15df54eb31a74c2
zakky <sadaoza@gmail.com>
2024-06-16 22:05:38 +0900

    types common

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,29 @@ import axios from "axios";
 import "./App.css";
 import { useState } from "react";
 import { Todo } from "./Todo";
-
-type TodoType = {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-};
+import { TodoType } from "./types/todo";
 
 function App() {
-const [todos, setTodos] = useState<Array<TodoType>>([]);
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
 
   const onClickFetchData = () => {
-    axios.get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos").then((res) => {
-    setTodos(res.data);
-    });
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
   };
 
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
-        <Todo title={todo.title} userId={todo.userId} completed={todo.completed} />
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
       ))}
     </div>
   );
--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,10 +1,6 @@
-type TodoType = {
-    userId: number;
-    title: string;
-    completed?: boolean;
-  };
+import { TodoType } from "./types/todo";
 
-export const Todo = (props: TodoType) => {
+export const Todo = (props: Omit<TodoType, "id">) => {
   const { title, userId, completed = false } = props;
   const completeMark = completed ? "【完】" : "【未】";
   return <p>{`${completeMark} ${title}(ユーザー:${userId})`}</p>;
